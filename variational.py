d00026e63eabd1f47ba75f64ff86d617e65bf755
unknown <ds16797@bristol.ac.uk>
2018-12-04 20:02:41 +0000

    evidence commit

--- variational.py
+++ variational.py
@@ -32,10 +32,14 @@ def p(a,v):
         res = 0
     return res
 
+def newp(a,v):
+    return (a + (v-a)/2)
+
+
 def l(image,i,j,x,y,v):
     res = 0
     sum = 0
-    for ne in neighbour.neighbours(i,j,x,y,8):
+    for ne in neighbour.neighbours(i,j,x,y,4):
         sum += p(image[ne],image[i][j])
     swap = sum/8
     if(image[i][j] == v):
@@ -50,42 +54,90 @@ def mfvb(image,iter):
     y = image.shape[1]
     #Initialise the variational distributions
     #Initialise M
-    m = np.zeros((x,y,iter+1),order='C')
-    mu = np.zeros((x,y,iter+1),order='C')
+    m = np.zeros((iter+1,x,y),order='C')
+    mu = np.ones((iter+1,x,y),order='C')
+
     q = np.zeros((x,y),order='C')
     #image is latent
 
+    #correct access is m[t,x,y]
     for t in range(iter):
         #[ii][jj] pair comprises the singular i iterator used in CW specification
+        print(t)
         for ii in range(x):
             for jj in range(y):
                 sum = 0
                 for ne in neighbour.neighbours(ii,jj,x,y,8):
                     #Add the -1..1 product value of neighbour with current, modified by the mu value for neighbour
-                    sum += (2*image[ne] -1) * (2*image[ii][jj] -1) * mu[ne[0],ne[1],t]
+                    sum += (2*image[ne] -1) * (2*image[ii][jj] -1) * mu[t,ne[0],ne[1]]
 
-                m[ii,jj,t+1] = 0
+                m[t+1,ii,jj] = sum
                 #mu = tanh(m_i^tau + 1/2(L_i(1) - L_i(-1)))
-                mu[ii,jj,t+1] = math.tanh(m[ii,jj,t] + 1/2*(p(image[ii][jj],1) - p(image[ii][jj],0)))
+                mu[t+1,ii,jj] = math.tanh(m[t,ii,jj] + 1/2*(newp(image[ii][jj],1) - newp(image[ii][jj],0)))
 
     qProb = 1
     for i in range(x):
         for j in range(y):
-            q[i,j] = sigm(2*(m[ii,jj,iter] + 1/2*(p(image[ii][jj],1) - p(image[ii][jj],0))))
+            q[i,j] = sigm(2*(m[iter,i,j] + 1/2*(newp(image[i][j],1) - newp(image[i][j],0))))
             qProb *= q[i,j]
 
     #returns variational distro and mu
-    return (mu,m)
+    return (q,mu,m)
 
 
 prop = 0.7
 varSigma = 0.1
 
-im = to_id(imageio.imread('pug.jpg'))
+im = to_1d(imageio.imread('pug.jpg'))
 #im[i] = [0..1]
 im = im/255
+x = im.shape[0]
+y = im.shape[1]
 
+fig = plt.figure()
+ax = fig.add_subplot(231)
+ax.imshow(im,cmap='gray')
 imG = imagebase.add_gaussian_noise(im,prop,varSigma)
+ax2 = fig.add_subplot(232)
+ax2.imshow(imG,cmap='gray')
 imS = imagebase.add_saltnpeppar_noise(im,prop)
-posterior = 0
-mu,m = mfvb(im,20)
+ax3 = fig.add_subplot(233)
+ax3.imshow(imS,cmap='gray')
+
+qS,muS,mS = mfvb(imS,7)
+qG,muG,mG = mfvb(imG,7)
+
+
+for i in range(x):
+    for j in range(y):
+        if(qS[i,j] < 0.5):
+            if(imS[i,j] >= 0.5):
+                imS[i,j] = newp(imS[i,j],0)
+            else:
+                imS[i,j] = newp(imS[i,j],1)
+        else:
+            if(imS[i,j] >= 0.5):
+                imS[i,j] = newp(imS[i,j],1)
+            else:
+                imS[i,j] = newp(imS[i,j],0)
+
+for i in range(x):
+    for j in range(y):
+        if(qG[i,j] < 0.5):
+            if(imG[i,j] >= 0.5):
+                imG[i,j] = newp(imG[i,j],0)
+            else:
+                imG[i,j] = newp(imG[i,j],1)
+        else:
+            if(imG[i,j] >= 0.5):
+                imG[i,j] = newp(imG[i,j],1)
+            else:
+                imG[i,j] = newp(imG[i,j],0)
+
+
+ax4 = fig.add_subplot(235)
+ax4.imshow(imG,cmap='gray')
+ax5 = fig.add_subplot(236)
+ax5.imshow(imS,cmap='gray')
+
+plt.savefig("vb.png")
